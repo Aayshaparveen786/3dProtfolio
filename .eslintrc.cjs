0bc998838532fcb4d75784f41fd2724ed50ca96b
Aayshaparveen <aaysha.parveen@ismartapps.co.uk>
2023-07-07 16:41:46 +0530

    Add CI in evn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, CI: false },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
